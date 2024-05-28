68dd4f9497c6e3b95f41cdb317828c0fdd28e3f3
sunao nakao <sunawo.m.lg@gmail.com>
2024-05-28 15:28:07 +0900

    比較演算子と論理演算子(修正)

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -10,9 +10,9 @@ public class ComparisonLogicalOperators {
 		System.out.println(isGreaterA);
 		
 		boolean isSunny = true;
-		boolean isWarm =true;
+		boolean isWarm = true;
 		
-		if (isSunny && isWarm){
+		if (isSunny && isWarm) {
 			System.out.println("true");
 		} else {
 			System.out.println("false");
@@ -22,7 +22,7 @@ public class ComparisonLogicalOperators {
 		int x = 1 ;
 		int y = 10 ;
 		boolean evenNumber = (y % 2 == 0);
-		boolean andResultA = (x >= 0 && evenNumber );
+		boolean andResultA = (x >= 0 && evenNumber);
 		System.out.println(andResultA);
 		
 		boolean hasPermission = !(false);
