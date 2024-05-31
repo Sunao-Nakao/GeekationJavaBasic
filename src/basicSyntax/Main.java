10228d3407b56f40a025efa160d02dcc152d3531
sunao nakao <sunawo.m.lg@gmail.com>
2024-05-31 10:43:55 +0900

    クラス

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,26 +3,17 @@ package basicSyntax;
 public class Main {
 
 	public static void main(String[] args) {
-		 System.out.println("こんにちは");
-		 System.out.println("私の名前は");
-		 System.out.println("Geek君です。");
-		 
-		 System.out.println("こんにちは" + "私の名前は" + "Geek君です。");
-		 
-		 System.out.println(1);
-		 System.out.println(-1);
-		 System.out.println(5 + 1);
-		 
-		 System.out.println("5 + 1");
-		 System.out.println("5" + "1");
-		 
-		 System.out.println(0.25);
-		 System.out.println(-5.0);
-		 System.out.println(5.0 - 0.25);
-		 
+		int age = 20;
+		double height = 100.5;
 		
-		 System.out.println(true);
-		 System.out.println(false);
+		GeekIntroduction.greeting(age, height);
+		
+		
+		
+		int num1 = 1;
+		int num2 = 30;
+		
+		GeekIntroduction.specialSkill(num1, num2);
 	}
 	
 }
